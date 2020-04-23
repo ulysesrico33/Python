8c98a639f431099e12927c9faafc257ca15185fe
Ulyses Rico <ulysesrico@Ulysess-MacBook-Pro.local>
2020-04-22 20:12:43 -0500

    Avanzando en machine learning...

--- Chatbot/main.py
+++ Chatbot/main.py
@@ -1,9 +1,22 @@
 # -*- coding: utf-8 -*-
-"""
-Spyder Editor
 
-This is a temporary script file.
-"""
 
-print("Hola")
-print("Comenzando el viaje...")
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+
+stemmer=LancasterStemmer()
+
+import numpy
+import tflearn
+import tensorflow
+import random
+import json
+
+with open("intent.json") as file:
+    data=json.load(file)
+    
+print(data)    
+
+print("So...?")
+    
+
