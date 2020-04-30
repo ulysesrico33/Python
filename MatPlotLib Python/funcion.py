b14bff3f06f8531b370c79fe2552c0f8dcabbc4e
Gl Ulyses Rico <ulyses.rico.2012@gmail.com>
2020-04-30 11:22:35 -0500

    Avances...

---
+++ MatPlotLib Python/funcion.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+#Primeros pasos con matpltplib
+
+
+import matplotlib.pyplot as plt
+
+
+
+def graficaA():
+    
+    a=[1,2,3,4]
+    b=[11,22,33,44]
+
+    plt.plot(a,b,color='blue',linewidth=3,label='linea')
+    plt.legend()
+    plt.show()
+    
+def graficaB():
+   
+    x1=[3,4,5,6]
+    y1=[5,6,3,4]
+    x2=[2,5,8]
+    y2=[3,4,3]
+    
+    plt.plot(x1,y1,label='línea 1',linewidth=4,color='blue')
+    plt.plot(x2,y2,label='línea 2',linewidth=4,color='green')
+    
+    plt.title('Diagrama de líneas')
+    plt.xlabel('Eje X')
+    plt.ylabel('Eje Y')
+    
+    plt.legend()
+    plt.grid()
+    plt.show()
+    
+    
+    
+    
+
+
+
+
+  
+
+
+              
+        
+        
+
+    
+
--- MatPlotLib Python/main.py
+++ MatPlotLib Python/main.py
@@ -1,15 +1,13 @@
 # -*- coding: utf-8 -*-
-#Primeros pasos con matpltplib
+#Main
 
 
-import matplotlib.pyplot as plt
+import funcion
 
-a=[1,2,3,4]
-b=[11,22,33,44]
 
-plt.plot(a,b,color='blue',linewidth=3,label='linea')
-plt.legend()
-plt.show()
+funcion.graficaA()
+funcion.graficaB()
+
 
 
 
