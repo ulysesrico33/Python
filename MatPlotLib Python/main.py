9b262cfa96092b1643ef927a08d28024c996b4dd
Gl Ulyses Rico <ulyses.rico.2012@gmail.com>
2020-04-30 09:49:06 -0500

    Creando archivos con matplot...

---
+++ MatPlotLib Python/main.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+#Primeros pasos con matpltplib
+
+
+import matplotlib.pyplot as plt
+
+a=[1,2,3,4]
+b=[11,22,33,44]
+
+plt.plot(a,b,color='blue',linewidth=3,label='linea')
+plt.legend()
+plt.show()
+
+
+
+  
+
+
+              
+        
+        
+
+    
+
